43f46c7ea1a2398cb34fa258a057e05c86dd0caf
Hyowon Choi <hyooowon0312@naver.com>
2026-08-18 18:38:23 +0900

    Delete codexHackathon-main directory

--- codexHackathon-main/app/page.tsx
+++
@@ -1,6 +0,0 @@
-import { SignalAtlasDashboard } from "@/components/signal-atlas-dashboard";
-import { liveFallback, marketEvents } from "@/lib/data";
-
-export default function Home() {
-  return <SignalAtlasDashboard events={marketEvents} initialLive={liveFallback} locale="en" />;
-}
